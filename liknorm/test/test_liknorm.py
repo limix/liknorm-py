7eccae78f3d5fbe79e40ac08eb2bb3a10ce52f08
Danilo Horta <danilo.horta@gmail.com>
2018-06-29 17:04:07 +0100

    testing for probit variance

--- liknorm/test/test_liknorm.py
+++ liknorm/test/test_liknorm.py
@@ -68,31 +68,46 @@ def test_liknormmachine_probit():
 
     y = (outcome, probit_variance)
     ceta = random.rand(5)
-    ctau = random.randn(5) * ceta
+    ctau = random.rand(5) * ceta
 
     lmom0 = empty(5, dtype=float64)
     hmu = empty(5, dtype=float64)
     hvar = empty(5, dtype=float64)
 
-    import pdb
-
-    pdb.set_trace()
     machine.moments(y, ceta, ctau, {"log_zeroth": lmom0, "mean": hmu, "variance": hvar})
 
     assert_allclose(
         lmom0,
-        [-3.478250e+00, -6.179203e+00, -4.473831e+00, -6.086356e+08, -7.042068e+00],
+        [
+            -1.0367561949160304,
+            -0.3766322328302855,
+            -0.14942751942053256,
+            -1.7575580439512268,
+            -0.1493594455614018,
+        ],
         atol=1e-7,
     )
     assert_allclose(
         hmu,
-        [1.952541e+00, -1.351837e+00, -6.763551e-01, 1.901923e+07, -1.438112e+00],
+        [
+            -4.354960549968583,
+            2.1155342209820724,
+            3.547896098810804,
+            -0.179262687618761,
+            3.296751923821712,
+        ],
         rtol=1e-5,
         atol=1e-5,
     )
     assert_allclose(
         hvar,
-        [2.008703e-01, 1.257172e-01, 6.619332e-02, 9.629856e+11, 9.355299e-02],
+        [
+            14.191471071807722,
+            2.7479536410029497,
+            4.422558883309547,
+            0.8254740377807543,
+            3.892092578099916,
+        ],
         atol=1e-7,
     )
 
