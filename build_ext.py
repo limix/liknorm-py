47ab8c100399b07621a71bc7b24fbcf5f6447648
Danilo Horta <danilo.horta@gmail.com>
2018-07-05 10:01:05 +0100

    new liknorm

--- build_ext.py
+++ build_ext.py
@@ -8,12 +8,12 @@ from cffi import FFI
 
 def windows_dirs(prefix, lib):
     dirs = []
-    if 'PROGRAMW6432' in os.environ:
-        fld = join(os.environ['PROGRAMW6432'], lib, prefix)
+    if "PROGRAMW6432" in os.environ:
+        fld = join(os.environ["PROGRAMW6432"], lib, prefix)
         if os.path.exists(fld):
             dirs += [fld]
-    if 'PROGRAMFILES' in os.environ:
-        fld = join(os.environ['PROGRAMFILES'], lib, prefix)
+    if "PROGRAMFILES" in os.environ:
+        fld = join(os.environ["PROGRAMFILES"], lib, prefix)
         if os.path.exists(fld):
             dirs += [fld]
     return dirs
@@ -21,27 +21,25 @@ def windows_dirs(prefix, lib):
 
 def windows_include_dirs():
     include_dirs = []
-    if 'INCLUDE' in os.environ:
-        include_dirs += [os.environ['INCLUDE']]
-    if 'LIBRARY_INC' in os.environ:
-        include_dirs += [os.environ['LIBRARY_INC']]
-    include_dirs += windows_dirs('include', 'liknorm')
+    if "INCLUDE" in os.environ:
+        include_dirs += [os.environ["INCLUDE"]]
+    if "LIBRARY_INC" in os.environ:
+        include_dirs += [os.environ["LIBRARY_INC"]]
+    include_dirs += windows_dirs("include", "liknorm")
     return include_dirs
 
 
 def windows_library_dirs():
     library_dirs = []
-    if 'LIBRARY_LIB' in os.environ:
-        library_dirs += [os.environ['LIBRARY_LIB']]
-    library_dirs += windows_dirs('lib', 'liknorm')
+    if "LIBRARY_LIB" in os.environ:
+        library_dirs += [os.environ["LIBRARY_LIB"]]
+    library_dirs += windows_dirs("lib", "liknorm")
     return library_dirs
 
 
 def windows_find_libname(lib, library_dirs):
-    names = [
-        "{}.lib".format(lib), "lib{}.lib".format(lib), "{}lib.lib".format(lib)
-    ]
-    folders = [f for ldir in library_dirs for f in ldir.split(';')]
+    names = ["{}.lib".format(lib), "lib{}.lib".format(lib), "{}lib.lib".format(lib)]
+    folders = [f for ldir in library_dirs for f in ldir.split(";")]
     for f in folders:
         for n in names:
             if os.path.exists(join(f, n)):
@@ -50,48 +48,28 @@ def windows_find_libname(lib, library_dirs):
     raise RuntimeError("{} library not found.")
 
 
-def prefix_liknorm_include(dirs):
-    if platform.system() == 'Windows':
-        sep = ';'
-    else:
-        sep = ':'
-
-    for d in dirs:
-        for folder in d.split(sep):
-            if os.path.exists(join(folder, 'liknorm', 'liknorm.h')):
-                return True
-
-    return False
-
-
 ffibuilder = FFI()
 
 folder = os.path.dirname(os.path.abspath(__file__))
 
-with open(join(folder, 'liknorm', 'interface.h'), 'r') as f:
+with open(join(folder, "liknorm", "interface.h"), "r") as f:
     ffibuilder.cdef(f.read())
 
-with open(join(folder, 'liknorm', 'interface.c'), 'r') as f:
+with open(join(folder, "liknorm", "interface.c"), "r") as f:
     interface_content = f.read()
 
-include_dirs = [join(get_config_var('prefix'), 'include')]
-library_dirs = [join(get_config_var('prefix'), 'lib')]
+include_dirs = [join(get_config_var("prefix"), "include")]
+library_dirs = [join(get_config_var("prefix"), "lib")]
 
-if platform.system() == 'Windows':
+if platform.system() == "Windows":
     include_dirs += windows_include_dirs()
     library_dirs += windows_library_dirs()
-    libraries = [
-        windows_find_libname('liknorm', library_dirs)
-    ]
+    libraries = [windows_find_libname("liknorm", library_dirs)]
 
 else:
-    libraries = ['liknorm']
-    include_dirs += ['/usr/include', '/usr/local/include']
-    library_dirs += ['/usr/lib', '/usr/local/lib']
-
-if prefix_liknorm_include(include_dirs):
-    interface_content = interface_content.replace(
-        "include \"liknorm.h\"", "include \"liknorm/liknorm.h\"")
+    libraries = ["liknorm"]
+    include_dirs += ["/usr/include", "/usr/local/include"]
+    library_dirs += ["/usr/lib", "/usr/local/lib"]
 
 ffibuilder.set_source(
     "liknorm.machine_ffi",
@@ -99,7 +77,8 @@ ffibuilder.set_source(
     libraries=libraries,
     library_dirs=library_dirs,
     include_dirs=include_dirs,
-    language='c')
+    language="c",
+)
 
 if __name__ == "__main__":
     ffibuilder.compile(verbose=True)
