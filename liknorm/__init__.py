9f1c7ad15324b1d5d89f443f87d06aa54309338d
Danilo Horta <danilo.horta@gmail.com>
2018-12-13 18:52:40 -0200

    format

--- liknorm/__init__.py
+++ liknorm/__init__.py
@@ -13,6 +13,6 @@ from __future__ import absolute_import as _
 from ._machine import LikNormMachine
 from ._testit import test
 
-__version__ = "1.1.13"
+__version__ = "1.1.14"
 
 __all__ = ["__version__", "test", "LikNormMachine"]
