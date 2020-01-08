212360a770411b141767d2c3e370ba6e7f00e235
Danilo Horta <danilo.horta@pm.me>
2020-01-08 17:37:14 +0000

    test command

--- build_ext.py
+++ build_ext.py
@@ -21,37 +21,17 @@ def _get_interface_c():
 
 ffibuilder = FFI()
 ffibuilder.cdef(_get_interface_h())
-libs = ["liknorm"]
-
-# if platform.system() == "Windows":
-#     win = Windows()
-#     progfiles = win.get_programfiles()
-#     for lib in libs:
-#         win.add_library_dir(join(progfiles, lib, "lib"))
-#         win.add_include_dir(join(progfiles, lib, "include"))
-
-#     libs = [win.find_libname(lib) for lib in libs]
-#     system: System = win
-# else:
-#     system = Unix()
-
-# library_dirs = system.get_library_dirs()
-# extra_link_args: List[str] = []
-# if platform.system() != "Windows":
-#     if len(library_dirs) > 0:
-#         extra_link_args += ["-Wl,-rpath,/usr/local/lib"]
 
 extra_link_args: List[str] = []
 if "LIKNORM_EXTRA_LINK_ARGS" in os.environ:
     extra_link_args += os.environ["LIKNORM_EXTRA_LINK_ARGS"].split(os.pathsep)
-
 
 ffibuilder.set_source(
     "liknorm._ffi",
     _get_interface_c(),
     extra_link_args=extra_link_args,
     language="c",
-    libraries=libs,
+    libraries=["liknorm"],
 )
 
 if __name__ == "__main__":
