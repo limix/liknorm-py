6b94fd849aa5b28538bae2f8425a24504a53d62f
Danilo Horta <danilo.horta@pm.me>
2025-01-06 14:38:18 +0000

    Update workflows, licensing, and versioning 🚀
    - Updated GitHub Actions workflow file `.github/workflows/test.yml`:
    - Simplified event triggers by removing `pull_request`.
    - Removed explicit OS versioning in matrix and updated checkout action.
    - Simplified poetry usage for testing.
    - Updated `LICENSE.md` with the current year (2024).
    - Removed unnecessary file `MANIFEST.in` and the script `ci/test`.
    - Revised `build_ext.py`:
    - Streamlined the build process and removed unused code.
    - Implemented a new helper function for integration.
    - Modified `liknorm/__init__.py` for bumping version `1.2.11`.
    - Adjusted `liknorm/_testit.py` by deprecating the `test` method.
    - Added a new header file `liknorm/include/liknorm.h`.
    - Updated `liknorm/interface.c` for header file consistency.
    - Bumped package version in `pyproject.toml` to `2.0.0`:
    - Updated dependencies and other settings to ensure compatibility.
    - Ensured all changes contribute to a streamlined build and better integration.

--- build_ext.py
+++ build_ext.py
@@ -1,118 +1,67 @@
 import os
-import platform
 import shutil
-import subprocess
-import sys
-import tarfile
-import urllib.request
+import sysconfig
 from pathlib import Path
-from typing import List
+from subprocess import check_call
+
+from cffi import FFI
+from git import Repo
 
 
-def rm(folder: Path, pattern: str):
-    for filename in folder.glob(pattern):
-        filename.unlink()
+def envlist(name: str) -> list[str]:
+    value = os.environ.get(name, None)
+    if value is None:
+        return []
+    return value.split(":")
 
 
-def get_cmake_bin():
-    from cmake import CMAKE_BIN_DIR
-
-    bins = [str(v) for v in Path(CMAKE_BIN_DIR).glob("cmake*")]
-    return str(sorted(bins, key=lambda v: len(v))[0])
+def uname():
+    return os.uname().sysname
 
 
-def darwin_setup():
-    os.environ.setdefault("MACOSX_DEPLOYMENT_TARGET", "11.0")
+def build_and_install(
+    root: Path, prefix: str, git_url: str, prj_dir: str, dst_dir: str, force: bool
+):
+    os.makedirs(root / ".gitdir", exist_ok=True)
+    if force or not (root / dst_dir).exists():
+        shutil.rmtree(root / ".gitdir" / dst_dir, ignore_errors=True)
+        Repo.clone_from(git_url, root / ".gitdir" / dst_dir, depth=1)
+        shutil.rmtree(root / dst_dir, ignore_errors=True)
+        shutil.move(root / ".gitdir" / dst_dir / prj_dir, root / dst_dir)
 
-    machine = platform.machine()
-    # Set by setuptools/cibuildwheels
-    archflags = os.environ.get("ARCHFLAGS")
-    if archflags is not None:
-        machine = ";".join(set(archflags.split()) & {"x86_64", "arm64"})
+    env = os.environ.copy()
+    env["C_INCLUDE_PATH"] = ":".join(envlist("C_INCLUDE_PATH") + [f"{prefix}/include"])
+    env["LIBRARY_PATH"] = ":".join(envlist("LIBRARY_PATH") + [f"{prefix}/lib"])
+    env["CFLAGS"] = "-std=c11 -O3 -fPIC"
+    env["PREFIX"] = prefix
 
-    os.environ.setdefault("CMAKE_OSX_ARCHITECTURES", machine)
+    if uname() == "Darwin" and "MACOSX_DEPLOYMENT_TARGET" not in env:
+        target = sysconfig.get_config_var("MACOSX_DEPLOYMENT_TARGET")
+        env["MACOSX_DEPLOYMENT_TARGET"] = target
 
-
-def build_deps(pwd: Path, user: str, project: str, version: str):
-    ext_dir = pwd / ".ext_deps"
-    shutil.rmtree(ext_dir, ignore_errors=True)
-
-    prj_dir = ext_dir / f"{project}-{version}"
-    build_dir = prj_dir / "build"
-    os.makedirs(build_dir, exist_ok=True)
-
-    url = f"https://github.com/{user}/{project}/archive/refs/tags/v{version}.tar.gz"
-
-    with urllib.request.urlopen(url) as rf:
-        data = rf.read()
-
-    tar_filename = f"{project}-{version}.tar.gz"
-
-    with open(ext_dir / tar_filename, "wb") as lf:
-        lf.write(data)
-
-    with tarfile.open(ext_dir / tar_filename) as tf:
-        tf.extractall(ext_dir)
-
-    if sys.platform == "darwin":
-        darwin_setup()
-
-    cmake_bin = get_cmake_bin()
-    subprocess.check_call(
-        [
-            cmake_bin,
-            "-S",
-            str(prj_dir),
-            "-B",
-            str(build_dir),
-            "-DCMAKE_BUILD_TYPE=Release",
-            "-DENABLE_ALL_WARNINGS=ON",
-            "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
-        ],
-    )
-    subprocess.check_call([cmake_bin, "--build", str(build_dir), "--config", "Release"])
-    subprocess.check_call(
-        [cmake_bin, "--install", str(build_dir), "--prefix", str(ext_dir)]
-    )
-    rm(ext_dir / "lib", "*.dylib")
-    rm(ext_dir / "lib", "*.so*")
-    rm(ext_dir / "lib64", "*.dylib")
-    rm(ext_dir / "lib64", "*.so*")
-
-
-def compile_extension():
-
-    from cffi import FFI
-
-    ffibuilder = FFI()
-
-    pwd = Path(os.path.dirname(os.path.abspath(__file__)))
-
-    if not os.getenv("LIKNORM_SKIP_BUILD_DEPS", False):
-        build_deps(pwd, "limix", "liknorm", "1.5.8")
-
-    with open(pwd / "liknorm" / "interface.h", "r") as f:
-        interface_h = f.read()
-
-    with open(pwd / "liknorm" / "interface.c", "r") as f:
-        interface_c = f.read()
-
-    extra_link_args: List[str] = []
-    if "LIKNORM_EXTRA_LINK_ARGS" in os.environ:
-        extra_link_args += os.environ["LIKNORM_EXTRA_LINK_ARGS"].split(os.pathsep)
-
-    ffibuilder.cdef(interface_h)
-    ffibuilder.set_source(
-        "liknorm._ffi",
-        interface_c,
-        libraries=["liknorm"],
-        extra_link_args=extra_link_args,
-        language="c",
-        library_dirs=[str(pwd / ".ext_deps" / "lib"), str(pwd / ".ext_deps" / "lib64")],
-        include_dirs=[str(pwd / ".ext_deps" / "include")],
-    )
-    ffibuilder.compile(verbose=True)
+    check_call(["make"], cwd=root / dst_dir, env=env)
+    check_call(["make", "install"], cwd=root / dst_dir, env=env)
 
 
 if __name__ == "__main__":
-    compile_extension()
+    CWD = Path(".").resolve()
+    TMP = CWD / ".build_ext"
+    PKG = CWD / "liknorm"
+
+    url = "https://github.com/limix/liknorm.git"
+    build_and_install(TMP, str(PKG), url, ".", "liknorm", True)
+
+    ffibuilder = FFI()
+
+    interface_c = open(PKG / "interface.c", "r").read()
+    ffibuilder.cdef(open(PKG / "interface.h", "r").read())
+    ffibuilder.set_source(
+        "liknorm._cffi",
+        interface_c,
+        language="c",
+        libraries=["liknorm"],
+        library_dirs=[str(PKG / "lib")],
+        include_dirs=[str(PKG / "include")],
+    )
+
+    ffibuilder.compile(verbose=True)
