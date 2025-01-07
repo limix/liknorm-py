b9bb3cb3e518391e2c55c4ee5fefee6f9b72cfe9
Danilo Horta <danilo.horta@pm.me>
2025-01-07 12:58:53 +0000

    test

--- build_ext.py
+++ build_ext.py
@@ -21,15 +21,12 @@ def uname():
     return os.uname().sysname
 
 
-def build_and_install(
-    root: Path, prefix: str, git_url: str, prj_dir: str, dst_dir: str, force: bool
-):
+def build_and_install(root: Path, prefix: str, git_url: str, dst_dir: str):
     os.makedirs(root / ".gitdir", exist_ok=True)
-    if force or not (root / dst_dir).exists():
-        shutil.rmtree(root / ".gitdir" / dst_dir, ignore_errors=True)
-        Repo.clone_from(git_url, root / ".gitdir" / dst_dir, depth=1)
-        shutil.rmtree(root / dst_dir, ignore_errors=True)
-        shutil.move(root / ".gitdir" / dst_dir / prj_dir, root / dst_dir)
+    shutil.rmtree(root / ".gitdir" / dst_dir, ignore_errors=True)
+    Repo.clone_from(git_url, root / ".gitdir" / dst_dir, depth=1)
+    shutil.rmtree(root / dst_dir, ignore_errors=True)
+    shutil.move(root / ".gitdir" / dst_dir, root / dst_dir)
 
     env = os.environ.copy()
     env["C_INCLUDE_PATH"] = ":".join(envlist("C_INCLUDE_PATH") + [f"{prefix}/include"])
@@ -41,6 +38,7 @@ def build_and_install(
         target = sysconfig.get_config_var("MACOSX_DEPLOYMENT_TARGET")
         env["MACOSX_DEPLOYMENT_TARGET"] = target
 
+    check_call(["ls"], cwd=root / dst_dir, env=env)
     check_call(["make"], cwd=root / dst_dir, env=env)
     check_call(["make", "install"], cwd=root / dst_dir, env=env)
 
@@ -51,7 +49,7 @@ if __name__ == "__main__":
     PKG = CWD / "liknorm"
 
     url = "https://github.com/limix/liknorm.git"
-    build_and_install(TMP, str(PKG), url, ".", "liknorm", True)
+    build_and_install(TMP, str(PKG), url, "liknorm")
 
     ffibuilder = FFI()
 
